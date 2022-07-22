59265c1e77262f1e7ecddd49b7532b96295e3ea4
Ayesha Arshad <ayeshaarshad@Ayeshas-MacBook-Pro.local>
2022-07-22 15:12:25 +0500

    Edit CSS

--- Js/form-validation.js
+++ Js/form-validation.js
@@ -1,8 +1,13 @@
+const alert=document.querySelector('.alert');
 document.querySelector('.form').addEventListener('submit', (event) => {
   const email = document.getElementById('email').value;
   if (email !== email.toLowerCase()) {
     event.preventDefault();
-    document.querySelector('.alert').innerHTML = "Email should be Lower Case!";
+    alert.innerHTML = "Email should be Lower Case!";
     event.target.reset();
+    alert.classList.add('active');
+  }
+  else{
+  alert.classList.remove('active');
   }
 });
