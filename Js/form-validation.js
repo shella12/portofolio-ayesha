95f984d0dfd8874e437f2ce21ce2a1d178b07a33
Ayesha Arshad <ayeshaarshad@Ayeshas-MacBook-Pro.local>
2022-07-22 16:41:14 +0500

    Fix linter errors

--- Js/form-validation.js
+++ Js/form-validation.js
@@ -1,13 +1,12 @@
-const alert=document.querySelector('.alert');
+const alert = document.querySelector('.alert');
 document.querySelector('.form').addEventListener('submit', (event) => {
   const email = document.getElementById('email').value;
   if (email !== email.toLowerCase()) {
     event.preventDefault();
-    alert.innerHTML = "Email should be Lower Case!";
+    alert.innerHTML = 'Email should be Lower Case!';
     event.target.reset();
     alert.classList.add('active');
-  }
-  else{
-  alert.classList.remove('active');
+  } else {
+    alert.classList.remove('active');
   }
 });
