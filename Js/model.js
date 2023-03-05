c31f9884cc0c58498e478a1262ec6cb4d2a7daf4
shella12 <ayeshaarshad4567@gmail.com>
2023-03-05 11:21:19 +0500

    Add project images

--- Js/model.js
+++ Js/model.js
@@ -6,7 +6,7 @@ const projects = [
     name: 'Aebinar',
     techTags: ['HTML', 'CSS', 'JavaScript'],
     imagesSrc: './images/project1.png',
-    imageArray: ['./images/project1.png', './images/project1.png', './images/project1.png', './images/project1.png'],
+    imageArray: ['./images/project1a.png', './images/project1b.png', './images/project1c.png', './images/project1d.png'],
     description: 'A website, to connect an audience and provide them with remote access to a platform for taking online seminars (webinar).'
     + '- Display a list of speakers.'
     + '- Display programs available.'
@@ -15,28 +15,28 @@ const projects = [
     seeSource: 'https://github.com/shella12/AebinAR',
   },
   {
-    name: 'Space X Hub',
-    techTags: ['CSS', 'React/Redux', 'RESTFUL APIs'],
-    imagesSrc: './images/project2.png',
-    imageArray: ['./images/project2.png', './images/project1.png', './images/project1.png', './images/project1.png'],
-    description: 'This is a group project I worked on as a Microverse student. This web app allows users to see available rockets for booking and missions they can join. Users can see their booked rockets and joined missions in the My-profile page and cancel any joined mission or booked rocket.',
-    seeLive: 'https://clever-hamster-fb01bd.netlify.app/',
-    seeSource: 'https://github.com/shella12/Space-X-hub',
-  },
-  {
     name: 'Air Quality Index (AQI)',
     techTags: ['CSS', 'React/Redux', 'RESTFUL APIs', 'Google APIs'],
-    imagesSrc: './images/project3.png',
-    imageArray: ['./images/project3.png', './images/project1.png', './images/project1.png', './images/project1.png'],
+    imagesSrc: './images/project2.png',
+    imageArray: ['./images/project2.png', './images/project2a.png', './images/project2b.png', './images/project2c.png'],
     description: 'This web app allows users to check for air quality in different cities of the world. In addition, it provides the air pollutants ratio in detail. The project uses "Air Pollution API" and "google place autocomplete " feature for the display of air quality data and fetching cities of the world.',
     seeLive: 'https://lighthearted-blancmange-f8080f.netlify.app/',
     seeSource: 'https://github.com/shella12/air-quality-index',
   },
   {
+    name: 'Poke Play Card',
+    techTags: ['HTML/CSS', 'JavaScript', 'RESTFUL APIs', 'Webpack'],
+    imagesSrc: './images/project3.png',
+    imageArray: ['./images/project3.png', './images/project3a.png', './images/project3b.png', './images/project3c.png'],
+    description: 'This project is built using PokeAPI for pokemon and Involvement API for interactions. In this project, you can view different Pokemons and their abilities. You can give a like to your favorite pokemon. You can also share your thoughts on the pokemon by commenting on it.',
+    seeLive: 'https://shella12.github.io/PokePlayCards.github.io/dist/',
+    seeSource: 'https://github.com/shella12/PokePlayCards.github.io',
+  },
+  {
     name: 'Book Store',
     techTags: ['CSS', 'React/Redux', 'RESTFUL APIs'],
     imagesSrc: './images/project4.png',
-    imageArray: ['./images/project4.png', './images/project1.png', './images/project1.png', './images/project1.png'],
+    imageArray: ['./images/project4.png', './images/project4a.png', './images/project4b.png', './images/project4c.png'],
     description: 'The Bookstore is a website made using React Redux and REST API. It is used for saving your books online. Book Store has the following features:'
     + '-  Display a list of books.'
     + '-  Add a book.'
@@ -45,19 +45,19 @@ const projects = [
     seeSource: 'https://github.com/shella12/reactBookStore',
   },
   {
-    name: 'Poke Play Card',
-    techTags: ['HTML/CSS', 'JavaScript', 'RESTFUL APIs', 'Webpack'],
+    name: 'Space X Hub',
+    techTags: ['CSS', 'React/Redux', 'RESTFUL APIs'],
     imagesSrc: './images/project5.png',
-    imageArray: ['./images/project5.png', './images/project1.png', './images/project1.png', './images/project1.png'],
-    description: 'This project is built using PokeAPI for pokemon and Involvement API for interactions. In this project, you can view different Pokemons and their abilities. You can give a like to your favorite pokemon. You can also share your thoughts on the pokemon by commenting on it.',
-    seeLive: 'https://shella12.github.io/PokePlayCards.github.io/dist/',
-    seeSource: 'https://github.com/shella12/PokePlayCards.github.io',
+    imageArray: ['./images/project5.png', './images/project5a.png', './images/project5b.png', './images/project5c.png'],
+    description: 'This is a group project I worked on as a Microverse student. This web app allows users to see available rockets for booking and missions they can join. Users can see their booked rockets and joined missions in the My-profile page and cancel any joined mission or booked rocket.',
+    seeLive: 'https://clever-hamster-fb01bd.netlify.app/',
+    seeSource: 'https://github.com/shella12/Space-X-hub',
   },
   {
     name: 'Math Magician',
     techTags: ['CSS', 'React'],
     imagesSrc: './images/project6.png',
-    imageArray: ['./images/project6.png', './images/project1.png', './images/project1.png', './images/project1.png'],
+    imageArray: ['./images/project6.png', './images/project6a.png', './images/project6b.png', './images/project6c.png'],
     description: '"Math magicians" is a basic calculator website made using react for all fans of mathematics. It is a Single Page App (SPA) that allows users to:'
     + "Make simple calculations, performs all math's basic functions, add, subtract, multiply and divide."
     + 'Read a random math-related quote using API.',
